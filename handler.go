9ef60fd65b16bba9421d38d85fdcc67fb6513fde
Kohta Takanami <ripplekouha@gmail.com>
2022-07-25 14:14:52 +0900

    [add] 課題1を回答

--- handler.go
+++ handler.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type Article struct {
+  Title string `json:"title"`
+}
+
+type Articles struct {
+  Articles []Article `json:"articles"`
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
   switch r.Method {
@@ -17,8 +26,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getDocuments(w http.ResponseWriter, r *http.Request) {
+  res, err := json.Marshal(Articles{[]Article{{"title"}}})
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    fmt.Fprintf(w, "An error occurred")
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
   w.WriteHeader(http.StatusOK)
-  fmt.Fprintf(w, "get document")
+  w.Write(res)
 
 }
 
